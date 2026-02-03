a691feadb9c56e7b72cc6192edefc7b2131805be
kidus <kidusefrem2@gmail.com>
2026-02-03 15:23:34 +0300

    Add employee data and endpoints for retrieving employee information

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var employees = new List<Employee>(){
+        new() { Id = 1, FirstName = "John", LastName = "Doe"},
+        new() {Id = 2 , FirstName = "Jane", LastName = "Doe"}
+};
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer(); // Needed for Swagger
@@ -15,6 +18,19 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapGet("/employees", );
+app.MapGet("/employees", () =>
+{
+    return Results.Ok(employees);
+} );
+app.MapGet("/employees/{id:int}", (int id) =>
+{
+    var employee = employees.SingleOrDefault(e=>e.Id == id);
+    if( employee == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(employee);
+    // return employee;
 
+});
 app.Run();
