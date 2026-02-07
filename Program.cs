77bd33e1836cee9f36f7ca6150442088dda36cc4
kidus <kidusefrem2@gmail.com>
2026-02-07 12:53:00 +0300

    Add Swagger support and employee management endpoints; set up test project with xUnit

--- Program.cs
+++ Program.cs
@@ -3,12 +3,15 @@ var employees = new List<Employee>(){
         new() { Id = 1, FirstName = "John", LastName = "Doe"},
         new() {Id = 2 , FirstName = "Jane", LastName = "Doe"}
 };
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer(); // Needed for Swagger
 builder.Services.AddSwaggerGen();           // <-- This was missing
 
 var app = builder.Build();
+var employeeRoute = app.MapGroup("employees");
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -18,11 +21,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapGet("/employees", () =>
+employeeRoute.MapGet(string.Empty, () =>
 {
     return Results.Ok(employees);
 } );
-app.MapGet("/employees/{id:int}", (int id) =>
+employeeRoute.MapGet("{id:int}", (int id) =>
 {
     var employee = employees.SingleOrDefault(e=>e.Id == id);
     if( employee == null)
@@ -32,5 +35,11 @@ app.MapGet("/employees/{id:int}", (int id) =>
     return Results.Ok(employee);
     // return employee;
 
+});
+employeeRoute.MapPost(string.Empty, (Employee employee) =>
+{
+    employee.Id = employees.Max(e=>e.Id)+1;
+    employees.Add(employee);
+    return Results.Created($"/employees/{employee.Id}", employee);
 });
 app.Run();
